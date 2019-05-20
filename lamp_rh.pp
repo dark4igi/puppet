8b552b44e656ad815ad87302a1da983561a470ca
root <root@master.example.com>
2019-05-20 13:29:47 +0000

    update

--- lamp_rh.pp
+++ lamp_rh.pp
@@ -1,11 +1,11 @@
 # execute 'apt-get update'
-exec { 'apt-update':                    # exec resource named 'apt-update'
+exec { 'update':                    # exec resource named 'apt-update'
   command => '/usr/bin/yum update -y'  # command this resource will run
 }
 
 # install apache2 package
 package { 'httpd':
-  require => Exec['apt-update'],        # require 'apt-update' before installing
+  require => Exec['update'],        # require 'apt-update' before installing
   ensure => installed,
 }
 
@@ -15,19 +15,19 @@ service { 'httpd':
 }
 
 # install mysql-server package
-#package { 'mysql-server':
-#  require => Exec['yum'],        # require 'apt-update' before installing
-#  ensure => installed,
-#}
+package { 'mariadb':
+  require => Exec['update'],        # require 'apt-update' before installing
+  ensure => installed,
+}
 
 # ensure mysql service is running
-#service { 'mysql':
-#  ensure => running,
-#}
+service { 'mariadb':
+  ensure => running,
+}
 
 # install php5 package
 package { 'php':
-  #require => Exec['yum'],        # require 'apt-update' before installing
+  require => Exec['update'],        # require 'apt-update' before installing
   ensure => installed,
 }
 
@@ -42,7 +42,7 @@ file { '/var/www/html/info.php':
 
 file { '/tmp/helloworld':
   ensure => present,
-  content => "Hello, world!\n",
+  content => "Hello, world!\n 123\n",
   mode => '0644',
   owner => 'ec2-user',
   group => 'ec2-user'
